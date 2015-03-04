f5c7a58636ebdb30cdb2228526a048d53b07d8ab
Brian Underwood <ml+github@semi-sentient.com>
2015-03-04 14:19:48 +0200

    Expire cache in three hours

--- lib/api_cache.rb
+++ lib/api_cache.rb
@@ -2,8 +2,8 @@ require 'active_support'
 
 ApiCache = if ENV['REDISCLOUD_URL']
              require 'redis-activesupport'
-             ActiveSupport::Cache::RedisStore.new(ENV['REDISCLOUD_URL'])
+             ActiveSupport::Cache::RedisStore.new(ENV['REDISCLOUD_URL'], expires_in: 3.hours)
            else
-             ActiveSupport::Cache::FileStore.new('cache')
+             ActiveSupport::Cache::FileStore.new('cache', expires_in: 3.hours)
            end
 
